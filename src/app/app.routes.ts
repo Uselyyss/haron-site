a2c13488217f544d569af26a2c18b02f250c8074
Maxim Dorogov <dorogow.maksim@yandex.ru>
2025-11-30 18:09:54 +0300

    feat(home): implement home component with gallery and service details
    - Add home component HTML structure including header, main content, and footer.
    - Implement CSS styles for gallery images, cards, and layout adjustments.
    - Create a test suite for the home component.
    - Add a favicon image for branding.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // редирект с '' на /home
+  { path: 'home', component: HomeComponent },
+];
